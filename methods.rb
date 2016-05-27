156379b3095f92fb9a39bf8a2d0a63cbdcc95d2a
Bill Stiber <billstiber@gmail.com>
2016-05-27 00:54:40 -0400

    add file methods.rb

---
+++ methods.rb
@@ -0,0 +1,10 @@
+#methods
+def names(a, b)
+  spouses = a + b
+  return " the married couples are #{a} and #{b}"
+end
+
+p names("john", "sally")
+p names("bran", "jane")
+p names("jack", "brandi")
+p names("brent", "peggy")
